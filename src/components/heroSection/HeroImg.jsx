e7cce00bd092a8ea48be42bdf0bd6ec86a93eae5
matin aliakbari <matin54843590@gmail.com>
2025-03-16 22:09:38 +0330

    changed images portfolio

--- src/components/heroSection/HeroImg.jsx
+++ src/components/heroSection/HeroImg.jsx
@@ -4,9 +4,9 @@ import { fadeIn } from "../../framerMotion/variants";
 
 const HeroImg = () => {
     return (
-        <div className="flex items-center justify-center">
+        <div className="flex items-center justify-center md:mt-14">
             <motion.div className="flex items-center justify-center h-full" variants={fadeIn('left', 0.2)} initial='hidden' whileInView='show' viewport={{ once: false, amount: 0 }}>
-                <img src="/images/matin-portfolio4.png" alt="hero Image" className="max-w-[650px] relative left-8 top-5 opacity-90 h-auto" />
+                <img src="/public/images/matin-portfoilio5.png" alt="hero Image" className="max-w-[550px] relative left-5 top-1.5 h-auto" />
                 <div className="absolute flex items-center justify-center -z-10 animate-pulse">
                     <PiHexagonThin className="md:h-[90%] sm:h-[120%] min-h-[600px] w-auto text-cyan blur-md animate-[spin_20s_linear_infinite] " />
                 </div>
