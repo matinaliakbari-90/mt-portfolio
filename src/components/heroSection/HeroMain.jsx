f5fc004abf4cd092641fc3099e14b3e90ef7c3ff
matin aliakbari <matin54843590@gmail.com>
2025-03-16 22:20:30 +0330

    change in heroMain

--- src/components/heroSection/HeroMain.jsx
+++ src/components/heroSection/HeroMain.jsx
@@ -6,7 +6,7 @@ import HeroBar from "./HeroBar"
 const HeroMain = () => {
     return (
         <>
-            <header className="pt-40 pb-16 md:pt-[5.5rem]">
+            <header className="pt-48 pb-16 md:pt-[5.5rem]">
                 <div className="flex items-center justify-between px-4 mx-auto max-w-[1250px] sm:flex-col md:flex-row relative">
                     <HeroText />
                     <HeroImg />
