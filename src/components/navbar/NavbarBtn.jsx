37d68b097be62843c0a797800f1b7a250710e99b
matin aliakbari <matin54843590@gmail.com>
2025-08-30 14:59:05 +0330

    fix in pdf my resume

--- src/components/navbar/NavbarBtn.jsx
+++ src/components/navbar/NavbarBtn.jsx
@@ -3,7 +3,7 @@ import MyResume from "../../../public/resume/MatinAliakbari-Resume.pdf";
 
 const NavbarBtn = () => {
     return (
-        <div className="border-[0.5px] border-cyan px-4 py-2 rounded-full font-body text-white text-xl font-bold cursor-pointer bg-gradient-to-r from-cyan bg-orange hover:shadow-cyanShadow hover:scale-110 transition-all duration-500">
+        <div className="border-[0.5px] border-cyan px-4 py-2 text-lg rounded-full font-body text-white md:text-xl font-bold cursor-pointer bg-gradient-to-r from-cyan bg-orange hover:shadow-cyanShadow hover:scale-110 transition-all duration-500">
             <a href={MyResume} download="MatinAliakbari-Resume.pdf" className="flex items-center gap-1">
                 My Resume 
                 <CgArrowBottomRight className="sm:hidden md:block" />
